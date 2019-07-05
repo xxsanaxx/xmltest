a5964f58fa49de69a06377c5cd861f87cfb4efba
xxsanaxx <52555413+xxsanaxx@users.noreply.github.com>
2019-07-05 17:00:32 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,4 @@
 (function(){
   'use strict'
-
-	$('#test').text('test');
-  // $.get('data/input.xml', function(xml){
-  //   var title = $(this).find("title").find("name").text();
-  //   alert(title);
-  // }
-	alert("test");
-
-  $.ajax({
-		url:"data/input.xml",
-		type:"get",
-		dataType:"xml",
-		timeout:1000,
-		cache:false,
-		error:function(){
-			alert("xmlファイルの読み込み失敗");
-			},
-		success:function(xml){
-			alert("xmlファイルの読み込み成功");
-      var title = $(this).find("title").find("name").text();
-      alert(title);
-			}
-		});
+  makeTable('https://xxsanaxx.github.io/xmltest/data/input.xml');
 })();
