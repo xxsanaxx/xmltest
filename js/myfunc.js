28ac6d6897025dd934fa349e0f85a5000725fd38
xxsanaxx <52555413+xxsanaxx@users.noreply.github.com>
2019-07-05 17:01:44 +0900

    Update myfunc.js

--- js/myfunc.js
+++ js/myfunc.js
@@ -1,10 +1,29 @@
 'use strict'
 //==================================================
-//  名前： myfunc
-//  概要：
-//  引数： なし
+//  名前： makeTable
+//  概要： サーバー上のXMLから表を作成する
+//  引数： path <i  > : XMLファイルのパス
 //  戻値： なし
 //==================================================
-function myfunc(){
+function makeTable(path){
+
+  var xml = loadXML(path);
+  var tbl = $('#res');
+  //----- 表を初期化する
+  tbl.empty();
+  //----- タイトル行を作成
+  var title = $('<tr>')
+  tbl.append(title);
+  $(xml).find("title *").each(function(i,val){
+    title.append('<td>'+$(val).text()+'</td>');
+  });
+  //----- データを作成
+  $(xml).find("ROOT result").each(function(i,val){
+    var data = $('<tr>').attr( 'id', "data" + i );
+    tbl.append(data);
+    $(val).find('*').each(function(j,val2){
+      data.append('<td>'+$(val2).text()+'</td>');
+    });
+  });
 
 }
